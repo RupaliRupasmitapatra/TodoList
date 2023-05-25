626b37489a262eaa7407772517faeaf4aa593304
RupaliRupasmitapatra <113132965+RupaliRupasmitapatra@users.noreply.github.com>
2023-05-25 11:54:01 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+      <div>
+        <h1>My App</h1>
+        <TodoList />
+      </div>
+  );
+};
+
+export default App;
